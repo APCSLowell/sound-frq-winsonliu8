53de661dced2486f2434af96b53d1905a578042f
winsonliu8 <146293819+winsonliu8@users.noreply.github.com>
2025-04-09 09:52:06 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -22,7 +22,7 @@ public class Sound
         samples[i] = limit; 
         numChanges++; 
       }
-      else if (samples[i] < -limit){
+      if (samples[i] < -limit){
         samples[i]= -limit; 
         numChanges++; 
       }
